e2ea2ce7666ae582ce0570aae7c93ec7f786d3b5
arielr52 <arielr52@gmail.com>
2014-09-04 13:13:15 -0400

    send emails

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,53 @@
+// http://www.nodemailer.com/
+// /npm install nodemailer
+var nodemailer = require('nodemailer');
 
-/*
- * GET home page.
- */
+var transporter = nodemailer.createTransport();
+
+var createEmailOptions = function(message) {
+	return {
+		from : 'Elana <elana@hotmail.com>', // sender address
+		to : 'arielr52@gmail.com, arielr52@gmail.com', // list of receivers
+		subject : 'Hello ', // Subject line
+		text : message, // plaintext body
+		html : '<b>' + message + '</b>' // html body
+	}
+};
 
 var kids = [];
 
-kids.push({ name: 'bob', email: 'bob@email' });
-kids.push({ name: 'anna', email: 'anna@email' });
+kids.push({
+	name : 'bob',
+	email : 'bob@email'
+});
+kids.push({
+	name : 'anna',
+	email : 'anna@email'
+});
 
-
-exports.index = function(req, res){
-    console.log(req.body.messageName);
-    console.log(req.body);
-	console.log("req="+req);
-	req.on('data', function(data) {
-		console.log("data="+data);	
+exports.index = function(req, res) {
+	console.log(req.body.messageName);
+	console.log(req.body);
+	console.log("req=" + req);
+	var messageName= req.body.messageName |''
+	var lastEmailMessage = 'none'
+	if (req.body.selectedKidName) {
+		lastEmailMessage = 'sent mail to the parent of '
+				+ req.body.selectedKidName
+		// send mail with defined transport object
+		transporter.sendMail(createEmailOptions(req.body.messageName),
+				function(error, info) {
+					if (error) {
+						console.log(error);
+					} else {
+						console.log('Message sent: ' + info.response);
+					}
+				});
+	}
+	res.render('index', {
+		title : 'Email for parents',
+		kids : kids,
+		messageName : messageName,
+		lastEmailMessage : req.body.selectedKidName
 	});
-  	
-  res.render('index', { title: 'Email for parents',kids: kids });
 };
--- routes/mail.js
+++ routes/mail.js
@@ -7,42 +7,29 @@ exports.edit = function(req, res) {
 	res.send("respond with a resource " + req.path);
 };
 
+
+
 /*
-//http://www.nodemailer.com/
-///npm install nodemailer
-var nodemailer = require('nodemailer');
-
-//create reusable transporter object using SMTP transport
+ * //http://www.nodemailer.com/ ///npm install nodemailer var nodemailer =
+ * require('nodemailer');
+ * 
+ * //create reusable transporter object using SMTP transport /* var transporter =
+ * nodemailer.createTransport({ service: 'Gmail', auth: { user:
+ * 'gmail.user@gmail.com', pass: 'userpass' } });
+ */
 /*
-var transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-      user: 'gmail.user@gmail.com',
-      pass: 'userpass'
-  }
-});
-*/
-/*
-var transporter = nodemailer.createTransport();
-
-//NB! No need to recreate the transporter object. You can use
-//the same transporter object for all e-mails
-
-//setup e-mail data with unicode symbols
-var mailOptions = {
-  from: 'Elana <elana@hotmail.com>', // sender address
-  to: 'ariel@gmail.com, ariel@gmail.com', // list of receivers
-  subject: 'Hello ', // Subject line
-  text: 'Hello world ', // plaintext body
-  html: '<b>Hello world</b>' // html body
-};
-
-//send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-  if(error){
-      console.log(error);
-  }else{
-      console.log('Message sent: ' + info.response);
-  }
-});
-*/
+ * var transporter = nodemailer.createTransport();
+ * 
+ * //NB! No need to recreate the transporter object. You can use //the same
+ * transporter object for all e-mails
+ * 
+ * //setup e-mail data with unicode symbols var mailOptions = { from: 'Elana
+ * <elana@hotmail.com>', // sender address to: 'ariel@gmail.com,
+ * ariel@gmail.com', // list of receivers subject: 'Hello ', // Subject line
+ * text: 'Hello world ', // plaintext body html: '<b>Hello world</b>' // html
+ * body };
+ * 
+ * //send mail with defined transport object transporter.sendMail(mailOptions,
+ * function(error, info){ if(error){ console.log(error); }else{
+ * console.log('Message sent: ' + info.response); } });
+ */
