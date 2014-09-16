c994c88562078f391d4625ae27722c329fe9d23e
arielr52 <arielr52@gmail.com>
2014-09-16 15:16:17 -0400

    css

--- mail/mail.js
+++ mail/mail.js
@@ -8,9 +8,15 @@ var options = {
 		compileDebug: false
 	}
 
+var arr1=['gluing', 'puzzies', 'running', 'running', 'speech', 'communication', 'coloring', ' building', 'jumping','takingTurns', ' paiting', 'dramaticPlay'];
+var arr2= ['throwCatch','sittingWithTheGroup','cuttingWithScissors','Manipulatives','climbing','painting']
+var arr= arr1.concat(arr2)
+var today = new Date().toDateString() 
 
 var send={"selectedKidName":"anna","other":"aa","books":"xxx","from":"","to":"","meals":"All","mealNote":"","Special Activity":""}
 options.att=send
+options.arr= arr
+options.today= today
 send.pretty=true
 var html = jade.renderFile('./letter.jade', options);
 console.log(html)
